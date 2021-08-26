baee41468da11bed201a5d743b9eec34fdb849c8
ProWily <williamyi@outlook.sa>
2021-08-26 15:23:57 +0300

    Add project files.

---
+++ MSExcel-CRUD/MSExcel-CRUD.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+namespace MSExcel_CRUD
+{
+    class MSExcel_CRUD
+    {
+
+        public static bool Create(string FileName,string FilePath)
+        {
+            return true;
+        }
+
+        public static bool Read(int row, int column)
+        {
+            return true;
+        }
+
+        public static bool Update(int row, int column)
+        {
+            return true;
+        }
+
+        public static bool Delete(int row, int column)
+        {
+            return true;
+        }
+
+
+    }
+}
---
+++ MSExcel-CRUD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MSExcel_CRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
